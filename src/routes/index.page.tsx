b7734269e75cca83ed83949763637bf4cc79cc9e
nitedani <nitedani@gmail.com>
2022-11-19 18:40:06 +0100

    update

--- src/routes/index.page.tsx
+++ src/routes/index.page.tsx
@@ -8,14 +8,14 @@ export default function HomePage() {
         height: "100vh",
       }}
     >
-      <Image src={image} width="80%" />
       <Image
         src={
           "https://images.hdqwalls.com/download/small-memory-8k-2a-7680x4320.jpg"
         }
         width={200}
-        height={200}
+
       />
+      <Image src={image} width="80%" />
       <Image
         src={
           "https://my4kwallpapers.com/wp-content/uploads/2021/06/Anime-Wallpaper.jpg"
