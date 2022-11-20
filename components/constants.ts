a97707c6addeb243aa5495720a2b23ec94c37f7c
nitedani <nitedani@gmail.com>
2022-11-20 19:24:57 +0100

    update

--- components/constants.ts
+++ components/constants.ts
@@ -1,4 +1,4 @@
-export const defaultSizes = [600, 1000, 1400, 2000, 2560, 3840];
+export const defaultSizes = [100, 300, 600, 1000, 1400, 2000, 2560, 3840];
 // this is a hard cap, not a target
 export const cacheSizeMb = 50;
 export const webpQuality = 65;
