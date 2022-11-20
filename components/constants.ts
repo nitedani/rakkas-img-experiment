08d08a6f2982d3fcb2f9cdbcf2297e9f4f15ef5e
nitedani <nitedani@gmail.com>
2022-11-20 06:01:34 +0100

    update

--- components/constants.ts
+++ components/constants.ts
@@ -1,4 +1,4 @@
 export const defaultSizes = [600, 1000, 1400, 2000, 2560, 3840];
 export const cacheSizeMb = 50;
-export const webpQuality = 70;
-export const avifQuality = 60;
+export const webpQuality = 65;
+export const avifQuality = 45;
